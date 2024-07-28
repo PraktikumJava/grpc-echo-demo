27dc88e5ff0c37659d744f8b41b87f3c2b744ea8
kpmy <petryxa.clever@gmail.com>
2024-07-28 15:03:39 +0300

    initial

---
+++ grpc-echo-client/src/test/java/ru/yandex/practicum/grpc/echo/client/GrpcEchoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.grpc.echo.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcEchoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ grpc-echo-server/src/test/java/ru/yandex/practicum/grpc/echo/server/GrpcEchoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.grpc.echo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcEchoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
